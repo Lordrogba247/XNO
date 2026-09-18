98014fb369f162d37c1c12e1df88a9fe17e933f3
Lordrogba247 <adedapomichael55@gmail.com>
2026-09-18 18:15:44 +0100

    favicon and access key

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import favicon from './assets/logo.png'
+import favicon from './assets/logo2.png'
 
 // Set favicon dynamically since it's processed by Vite
 const link = document.querySelector("link[rel~='icon']") || document.createElement('link');
